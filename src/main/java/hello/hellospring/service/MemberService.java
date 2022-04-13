a148596f4d067a14c0445b8eabe75a9ef0bbac97
fDevJc <fDevJc@gmail.com>
2022-04-13 19:50:15 +0900

    스프링 빈과 의존관계/자바코드로 직접 빈등록1

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,14 +8,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Autowired
-    public MemberService(MemoryMemberRepository memoryMemberRepository) {
-        memberRepository = memoryMemberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     public Long join(Member member) {
